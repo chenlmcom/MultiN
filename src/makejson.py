bc17e617cdd51587f1e218ab1e7c33e972c198ab
chenlmcom <chenlmnet@126.com>
2021-02-05 14:38:02 +0800

    添加一些快捷键 添加Multi_N1模式

--- src/makejson.py
+++ src/makejson.py
@@ -5,6 +5,34 @@ import copy
 
 allModifiers = ['caps_lock','left_command','left_control','left_option','left_shift','right_command','right_control','right_option','right_shift','fn','command','control','option','shift','left_alt','left_gui','right_alt','right_gui','any']
 
+keyboards = [
+  ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' ],
+  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '⌫'],
+  ['⇥', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+  ['⇪', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '⏎'],
+  ['⇧', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '. ', '/', '⇧'],
+  ['fn', '⌃', '⌥', '⌘', ' ', '⌘', '⌥', '←', '↑', '↓', '→'],
+]
+
+keyMap = {
+  'comma': ',',
+  'period': '. ',
+  'hyphen': '-',
+  'equal_sign': '=',
+  'grave_accent_and_tilde': '`',
+  'spacebar': ' '
+}
+
+keySpace = {
+  '113': [1, 1], # ⌫
+  '20': [1, 1], # ⇥
+  '30': [2, 1], # ⇪
+  '312': [1, 1], # ⏎
+  '40': [2, 2], # 左⇧
+  '411': [2, 2], # 右⇧
+  '54': [4, 4], # Spacebar
+}
+
 def genManTos(to, modifiers):
   """
   生成manipulators to_event
@@ -131,73 +159,193 @@ def manipulator(key, modifiers, manTos, conditions):
   manipulator["from"] = fromKeys
   return manipulator
 
+def makeManipulators(key, to, conditions):
+  manipulators = []
+  froms = key.split('-') # 以-分割，
+  if len(froms) >= 1:
+    key = froms[len(froms) -1] # 最后一项为固定按键
+    f = key
+    t = to
+    if to.find(";") != -1:
+      tos = to.split(";")
+      if len(tos) == 2:
+        to = tos[0]
+        t = tos[1]
+      elif len(tos) == 3:
+        to = tos[0]
+        f = tos[1]
+        t = tos[2]
+    # print(sec + ":" + f + " to: " + t)
+    dealKeyboard(secKeyboard, f, t)
+    if len(froms) > 1: # 有同步按键时
+      syncModifiers = combine(froms[0:len(froms) - 1]) # 前面几项为同步按键组合
+      syncModifiers.append([])
+      for syncModifier in syncModifiers:
+        manTos = genManTos(to, syncModifier)
+        modifiers = copy.deepcopy(syncModifier)
+        manipulators.append(manipulator(key, modifiers, manTos, conditions))
+    else:
+      manTos = genManTos(to, [])
+      manipulators.append(manipulator(key, [], manTos, conditions))
+  return manipulators
+
+def keyboard(keys):
+  keyboard = "@startsalt\n{\n"
+  for i in range(len(keys)):
+    lineKeys = keys[i]
+    for idx in range(len(lineKeys)):
+      key = lineKeys[idx]
+      spaceKey = str(i) + '' + str(idx)
+      if spaceKey in keySpace:
+        space = keySpace[spaceKey]
+        key = "".join([" "]*space[0]) + key + "".join([" "]*space[1])
+      if key == '⇥' or key == '⌫' or key == '⏎':
+        key = ' ' + key + ' '
+      elif key == '⇪':
+        key = '  ' + key + ' '
+      elif key == '⇧':
+        key = '  ' + key + '  '
+      elif key == ' ':
+        key = '         '
+      if key == '.':
+        pass
+      elif len(key) == 1:
+        lineKeys[idx] = "[ " + key + " ]|*"
+      elif len(key) == 2:
+        lineKeys[idx] = "[ " + key + "]|*"
+      else:
+        lineKeys[idx] = "["
+        for i in range(int(len(key)/2)):
+          lineKeys[idx] += " "
+        lineKeys[idx] += key
+        for i in range(int(len(key)/2)):
+          lineKeys[idx] += " "
+        lineKeys[idx] += "]"
+        for i in range(len(key) - 1):
+          lineKeys[idx] += "|*"
+    keyboard += "|".join(lineKeys)
+    keyboard += '\n'
+  keyboard += '}\n@endsalt'
+  return keyboard
+
+def dealKeyboard(keys, f, t):
+  for lineKeys in keys:
+    for idx in range(len(lineKeys)):
+      key = lineKeys[idx]
+      if f in keyMap:
+        f = keyMap[f]
+      if key == f:
+        lineKeys[idx] = t
+
 configFile = 'config.ini'
 distFile = '../dist/multi_n.json'
+keyboardDir = '../dist/keyboards/'
 
 cf = configparser.ConfigParser()
 cf.read(configFile)
 
 print('---- 生成Karabiner_Elemnets配置文件 -----')
-data = {}
+data = {
+  "title" : "",
+  "maintainers" : [],
+  "rules" : []
+}
 secs = cf.sections()
 # print(secs)
 for sec in secs:
   print('------ section: ' + sec + ' --------')
   options = cf.options(sec)
   print(options)
-  if (sec == 'MultiN'):
-    data['title'] = cf.get('MultiN', 'title')
-    data['maintainers'] = [cf.get('MultiN', 'maintainers')]
-  else:
-    if 'rules' not in data:
-      data['rules'] = []
-    conditions = []
-    options = cf.options(sec)
+  secKeyboard = copy.deepcopy(keyboards)
+  options = cf.options(sec)
 
+  # manipulators
+  manipulators = []
+  conditions = []
+  for option in options:
     # conditions
-    if 'apps' in options:
+    if option in ['desc']:
+      continue
+    elif option == 'title':
+      data['title'] = cf.get('MultiN', 'title')
+    elif option == 'maintainers':
+      data['maintainers'] = [cf.get('MultiN', 'maintainers')]
+    elif option in ['apps']:
       conditions.append({
         "type": "frontmost_application_if",
-        "bundle_identifiers": cf.get(sec, 'apps').split(',')
+        "bundle_identifiers": cf.get(sec, option).split(',')
       })
-
-    for option in options:
-      if option in ['total', 'lower_half_area', 'upper_half_area', 'left_half_area', 'right_half_area']:
-        conditions.append({
+    elif option in ['total', 'lower_half_area', 'upper_half_area', 'left_half_area', 'right_half_area']:
+      conditions.append({
+        "type": "variable_if",
+        "name": "multitouch_extension_finger_count_" + option,
+        "value": int(cf.get(sec, option))
+      })
+    elif option in ['mode-key']:
+      modeKey = cf.get(sec, option).split(',')[0]
+      modeKeyNext = modeKey
+      modeKeyPrev = ""
+      if modeKey.find(":") != -1:
+        modeKeyNext = modeKey.split(":")[0]
+        modeKeyPrev = modeKey.split(":")[1]
+      modeValue = int(cf.get(sec, option).split(',')[1])
+      for mode in range(modeValue + 1):
+        nextMode = str((mode + 1) % (modeValue + 1))
+        prevMode = str((mode + modeValue) % (modeValue + 1))
+        # print('mode:' + str(mode) + ":" + nextMode + ":" + prevMode)
+        modConditions = []
+        modConditions.extend(conditions)
+        modConditions.append({
           "type": "variable_if",
-          "name": "multitouch_extension_finger_count_" + option,
-          "value": int(cf.get(sec, option))
+          "name": "multi_mode",
+          "value": mode
+        })
+        modConditions.append({
+          "type": "variable_if",
+          "name": "multitouch_extension_finger_count_total",
+          "value": 0
         })
 
-    # manipulators
-    manipulators = []
-    for option in options:
-      if option in ['total', 'desc', 'apps', 'lower_half_area', 'upper_half_area', 'left_half_area', 'right_half_area']:
-        continue
-      to = cf.get(sec, option)
-      froms = option.split('-') # 以-分割，
-      if len(froms) >= 1:
-        key = froms[len(froms) -1] # 最后一项为固定按键
-        if len(froms) > 1: # 有同步按键时
-          syncModifiers = combine(froms[0:len(froms) - 1]) # 前面几项为同步按键组合
-          syncModifiers.append([])
-          for syncModifier in syncModifiers:
-            manTos = genManTos(to, syncModifier)
-            modifiers = copy.deepcopy(syncModifier)
-            manipulators.append(manipulator(key, modifiers, manTos, conditions))
-        else:
-          manTos = genManTos(to, [])
-          manipulators.append(manipulator(key, [], manTos, conditions))
-    # rule
-    desc = ""
-    if 'desc' in options:
-      desc = cf.get(sec, 'desc')
-    rule = {
-      "description": desc,
-      "available_since": "12.6.9",
-      "manipulators": manipulators
-    }
-    data['rules'].append(rule)
+        modeNextManipulator = makeManipulators(modeKeyNext,
+          "set:multi_mode." + nextMode + ",left_command left_shift left_control left_option " + nextMode,
+          modConditions)[0]
+        modeNextManipulator['to_if_alone'] = modeNextManipulator['to']
+        modeNextManipulator['to'] = { "key_code": modeKeyNext }
+        manipulators.append(modeNextManipulator)
+
+        modePrevManipulator = makeManipulators(modeKeyPrev,
+          "set:multi_mode." + prevMode + ",left_command left_shift left_control left_option " + prevMode,
+          modConditions)[0]
+        modePrevManipulator['to_if_alone'] = modePrevManipulator['to']
+        modePrevManipulator['to'] = { "key_code": modeKeyPrev }
+        manipulators.append(modePrevManipulator)
+
+        modePrevManipulator = makeManipulators('esc',
+          "set:multi_mode." + prevMode + ",left_command left_shift left_control left_option " + prevMode,
+          modConditions)[0]
+        modePrevManipulator['to_if_alone'] = modePrevManipulator['to']
+        modePrevManipulator['to'] = { "key_code": modeKeyPrev }
+        manipulators.append(modePrevManipulator)
+    elif option in ['mode']:
+      conditions.append({
+        "type": "variable_if",
+        "name": "multi_mode",
+        "value": int(cf.get(sec, option))
+      })
+    else:
+      manipulators.extend(makeManipulators(option, cf.get(sec, option), conditions))
+      
+  # rule
+  desc = ""
+  if 'desc' in options:
+    desc = cf.get(sec, 'desc')
+  rule = {
+    "description": desc,
+    "available_since": "12.6.9",
+    "manipulators": manipulators
+  }
+  data['rules'].append(rule)
+  print(keyboard(secKeyboard), file = open(keyboardDir + sec + '.puml', "w"))
 
 # 打开文件以便写入
 f = open(distFile, "w")
